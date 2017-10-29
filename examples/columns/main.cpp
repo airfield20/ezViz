088eda0c4c364e5e68497cb0ceff371d2796eabf
sarlab <afcofiel@umich.edu>
2017-10-29 18:37:57 -0400

    Working columns example

--- examples/columns/main.cpp
+++ examples/columns/main.cpp
@@ -1,1 +1,20 @@
-int main(){}
+#include "../../ezViz.hpp"
+
+#define NUM_COLUMNS 4
+
+int main(){
+  Gui gui; //initialize user interface
+
+  gui.columns(NUM_COLUMNS); //specify to create columns interface and number of columns
+
+  gui.updateHeader(1, "First column header");
+  gui.updateHeader(3, "This is an example");
+
+  //gui.updateContents(1, "Write a string here");
+  gui.updateContents(2, 46.005);
+  gui.updateContents(3, "Information can be overwritten here");
+  gui.updateContents(1, "Ctrl-C to exit");
+
+  while(true){};
+
+}
