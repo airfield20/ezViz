5e99d097981210c9d548db2a5dc9429cb183b11e
sarlab <afcofiel@umich.edu>
2017-10-29 14:29:02 -0400

    columns example added (not working)

---
+++ examples/columns/CMakeFiles/columns.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/columns.dir/main.o"
+  "columns.pdb"
+  "columns"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/columns.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
